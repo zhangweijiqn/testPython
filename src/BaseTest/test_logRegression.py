ac6ad794525a14dd898659d6897158048c8741e1
zhangwj <zhangweijian@jd.com>
2016-08-04 18:26:33 +0800

    add comments

--- src/BaseTest/test_logRegression.py
+++ src/BaseTest/test_logRegression.py
@@ -25,12 +25,12 @@ def trainLogRegres(train_x, train_y, opts):
     # optimize through gradient descent algorilthm
     for k in range(maxIter):
         if opts['optimizeType'] == 'gradDescent': # gradient descent algorilthm
-            output = sigmoid(train_x * weights)
-            error = train_y - output
-            weights = weights + alpha * train_x.transpose() * error
+            output = sigmoid(train_x * weights) #vector
+            error = train_y - output            #vector
+            weights = weights + alpha * train_x.transpose() * error     # dot between vector train_x and error according to formula
         elif opts['optimizeType'] == 'stocGradDescent': # stochastic gradient descent
             for i in range(numSamples):
-                output = sigmoid(train_x[i, :] * weights)
+                output = sigmoid(train_x[i, :] * weights)    # numpy用法，取[ [],[],[]]中元素
                 error = train_y[i, 0] - output
                 weights = weights + alpha * train_x[i, :].transpose() * error
         elif opts['optimizeType'] == 'smoothStocGradDescent': # smooth stochastic gradient descent
@@ -89,8 +89,6 @@ def showLogRegres(weights, train_x, train_y):
     plt.show()
 
 
-########## test ############
-
 def loadData():
     train_x = []
     train_y = []
@@ -101,22 +99,19 @@ def loadData():
         train_y.append(float(lineArr[2]))
     return mat(train_x), mat(train_y).transpose()
 
+########## test ############
 
-## step 1: load data
-print "step 1: load data..."
-train_x, train_y = loadData()
-test_x = train_x; test_y = train_y
+if __name__ == "__main__":
 
-## step 2: training...
-print "step 2: training..."
-opts = {'alpha': 0.01, 'maxIter': 20, 'optimizeType': 'smoothStocGradDescent'}
-optimalWeights = trainLogRegres(train_x, train_y, opts)
+    ## step 1: load data
+    print "step 1: load data..."
+    train_x, train_y = loadData()
+    test_x = train_x; test_y = train_y
 
-## step 3: testing
-print "step 3: testing..."
-accuracy = testLogRegres(optimalWeights, test_x, test_y)
+    ## step 2: training...
+    print "step 2: training..."
+    opts = {'alpha': 0.01, 'maxIter': 20, 'optimizeType': 'gradDescent'}
+    optimalWeights = trainLogRegres(train_x, train_y, opts)
 
-## step 4: show the result
-print "step 4: show the result..."
-print 'The classify accuracy is: %.3f%%' % (accuracy * 100)
-showLogRegres(optimalWeights, train_x, train_y)
+
+
