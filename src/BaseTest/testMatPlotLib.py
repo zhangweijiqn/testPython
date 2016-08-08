49303924bdb60af3a50d696092696221c731cccc
zhangwj <zhangweijian@jd.com>
2016-08-08 20:21:03 +0800

    test CNN

--- src/BaseTest/testMatPlotLib.py
+++ src/BaseTest/testMatPlotLib.py
@@ -62,14 +62,92 @@ def testDynamic():
         lines = ax.plot(x_data, y_data, 'r-', lw=3)
         plt.pause(0.2)
 
-def baseTest():
+def baseTest1():
+    #散点图
     s0 = np.arange(-500,500,1)
-    s1 = np.random.randn(1000)
     s2 = np.random.randn(1000)/np.sqrt(1000)
 
-    plt.scatter(s0, s1)
+    plt.scatter(s0, s2)
+    plt.show()
+
+def baseTest2():
+    #一条曲线, plot函数（常用）
+    # Compute the x and y coordinates for points on a sine curve
+    x = np.arange(0, 3 * np.pi, 0.1)
+    y = np.sin(x)
+
+    # Plot the points using matplotlib
+    plt.plot(x, y)
+    plt.show()  # You must call plt.show() to make graphics appear.
+
+def baseTest3():
+    #多条曲线，plot函数
+    import numpy as np
+    import matplotlib.pyplot as plt
+
+    # Compute the x and y coordinates for points on sine and cosine curves
+    x = np.arange(0, 3 * np.pi, 0.1)
+    y_sin = np.sin(x)
+    y_cos = np.cos(x)
+
+    # Plot the points using matplotlib
+    plt.plot(x, y_sin)
+    plt.plot(x, y_cos)
+    plt.xlabel('x axis label')
+    plt.ylabel('y axis label')
+    plt.title('Sine and Cosine')
+    plt.legend(['Sine', 'Cosine'])
+    plt.show()
+
+def testSubplot():
+    #You can plot different things in the same figure using the subplot function. Here is an example:
+    import numpy as np
+    import matplotlib.pyplot as plt
+
+    # Compute the x and y coordinates for points on sine and cosine curves
+    x = np.arange(0, 3 * np.pi, 0.1)
+    y_sin = np.sin(x)
+    y_cos = np.cos(x)
+
+    # Set up a subplot grid that has height 2 and width 1,
+    # and set the first such subplot as active.
+    plt.subplot(2, 1, 1)
+
+    # Make the first plot
+    plt.plot(x, y_sin)
+    plt.title('Sine')
+
+    # Set the second subplot as active, and make the second plot.
+    plt.subplot(2, 1, 2)
+    plt.plot(x, y_cos)
+    plt.title('Cosine')
+
+    # Show the figure.
+    plt.show()
+
+def testImages():
+    #You can use the imshow function to show images.
+    import numpy as np
+    from scipy.misc import imread, imresize
+    import matplotlib.pyplot as plt
+
+    img = imread('../resources/cat.jpg')
+    print img.shape,img
+    img_tinted = img * [1, 0.95, 0.9]
+
+    # Show the original image
+    plt.subplot(1, 2, 1)
+    plt.imshow(img)
+
+    # Show the tinted image
+    plt.subplot(1, 2, 2)
+
+    # A slight gotcha with imshow is that it might give strange results
+    # if presented with data that is not uint8. To work around this, we
+    # explicitly cast the image to uint8 before displaying it.
+    plt.imshow(np.uint8(img_tinted))
     plt.show()
 
 if __name__=='__main__':
     # testDynamic()
-    baseTest()
+    testImages()
