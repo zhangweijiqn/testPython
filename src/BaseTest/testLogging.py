535888c21bf55633f46e8764429e71d716acf5ef
zhangweijian01 <zhangweijian01@baidu.com>
2017-03-22 11:04:21 +0800

    add logging test

--- src/BaseTest/testLogging.py
+++ src/BaseTest/testLogging.py
@@ -1,3 +1,4 @@
+#coding=utf8
 import logging
 
 # 日志级别大小关系为：CRITICAL > ERROR > WARNING > INFO > DEBUG > NOTSET，当然也可以自己定义日志级别。
@@ -6,7 +7,7 @@ import logging
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
                     datefmt='%a, %d %b %Y %H:%M:%S',
-                    filename='myapp.log',
+                    filename='log.txt',
                     filemode='w')   #可以改写到配置文件中
 
 logging.debug('This is debug message')
