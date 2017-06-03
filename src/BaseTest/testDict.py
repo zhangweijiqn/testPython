5a1a8bf7f1d0d56b80dacfe4d1cbf42e7b089065
zhangweijian01 <zhangweijian01@baidu.com>
2017-06-03 14:18:38 +0800

    add test for dict

--- src/BaseTest/testDict.py
+++ src/BaseTest/testDict.py
@@ -18,6 +18,11 @@ sorted(d.items(), lambda x, y: cmp(x[1], y[1]), reverse=True)
 # sort according to key
 [(k, d[k]) for k in sorted(d.keys(), reverse=True)]
 
+# dict value is a list ,sort according to list elem
+tids = {1: [1, 0.53], 2: [2, 0.56], 3: [3, 0.89]}
+sorted_tids = sorted(tids.items(), lambda x, y: cmp(x[1][1], y[1][1]), reverse=True)
+sorted_tids2 = sorted(tids.items(), lambda x, y: cmp(0.5*x[1][0]+2*x[1][1], 0.5*y[1][0]+2*y[1][1]), reverse=True)
+
 # delete key
 x = {1: 2, 3: 4, 4: 3, 2: 1, 0: 0}
 x.pop(1)
